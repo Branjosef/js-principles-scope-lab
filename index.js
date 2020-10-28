eb66437328c29d6c0da0ea6df06f3a73ffd95f39
Brandon Fonnemann <branjosef@gmail.com>
2020-10-28 11:21:25 -0500

    commit tree b7178b0cd49d52f4f96de3c1d78de709f1796d46

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+
+var customerName = 'bob'; 
+
+function upperCaseCustomerName() {
+  return customerName = customerName.toUpperCase();
+};
+
+function setBestCustomer(){
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(text){
+  bestCustomer = text;
+};
+
+const leastFavoriteCustomer = 'brian';
+
+function changeLeastFavoriteCustomer(text){
+  leastFavoriteCustomer = text;
+}
